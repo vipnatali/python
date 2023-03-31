a25b9a9777619ee8314e6569b0148388a9f18e52
Natali <vipnatalii@bk.ru>
2023-03-31 20:47:48 +0500

    Сохранили все задачи

---
+++ tasck2.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая на вход принимает 5 чисел
+# и находит максимальное из них
+
+nam1 = int(input())
+nam2 = int(input())
+nam3 = int(input())
+nam4 = int(input())
+nam5 = int(input())
+max = nam1
+
+if nam2 > max:
+    max = nam2
+if nam3 > max:
+    max = nam3
+if nam4 > max:
+    max = nam4
+if nam5 > max:
+    max = nam5
+print(max) 
+
